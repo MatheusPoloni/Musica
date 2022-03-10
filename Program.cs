f90362dbd040b24d3f8faf05583a2c6dc9665d93
matheus guilherme <machadomatheus0000@gmail.com>
2022-03-10 16:33:20 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Console.ForegroundColor = ConsoleColor .DarkYellow;
+Console.WriteLine("Shawn Mendes - It'll Be Okay");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Are we gonna make it?");
+Console.WriteLine("Is this gonna hurt?");
+Console.WriteLine("Oh, we can try to sedate it");
+Console.WriteLine("But that never works");
+Console.WriteLine("Yeah");
+Console.WriteLine();
+
+Console.WriteLine("I start to imagine a world where we don't collide");
+Console.WriteLine("It's making me sick, but we'll heal and the sun will rise");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor .DarkRed;
+Console.WriteLine("If you tell me you're leaving, I'll make it easy");
+Console.WriteLine("It'll be okay");
+Console.WriteLine("If we can't stop the bleeding");
+Console.WriteLine("We don't have to fix it, we don't have to stay");
+Console.WriteLine("I will love you either way");
+Console.WriteLine("Ooh-ooh, it'll be oh, be okay");
+Console.WriteLine("Ooh-ooh");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Oh, the future we dreamed of is fading to black");
+Console.WriteLine("Oh");
+Console.WriteLine("Oh, there's nothing more painful");
+Console.WriteLine("Nothing more painful, oh-woah");
+Console.WriteLine();
+
+Console.WriteLine("I start to imagine a world where we don't collide");
+Console.WriteLine("And it's making me sick, but we'll heal and the sun will rise");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor .DarkRed;
+Console.WriteLine("If you tell me you're leaving, I'll make it easy");
+Console.WriteLine("It'll be okay (It'll be okay)");
+Console.WriteLine("And if we can't stop the bleeding");
+Console.WriteLine("We don't have to fix it, we don't have to stay (don't have to stay)");
+Console.WriteLine("I will love you either way");
+Console.WriteLine("Ooh-ooh, it'll be oh, be okay");
+Console.WriteLine("Ooh-ooh");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("I will love you either way");
+Console.WriteLine("It might be so sweet");
+Console.WriteLine("It might be so bittee");
+Console.WriteLine("I will love you either way");
+Console.WriteLine("It might be so sweet");
+Console.WriteLine("It might be so bitter (ooh-ooh)");
+Console.WriteLine();
+
+Console.WriteLine("Oh, if the future we've dreamed of is fading to black");
+Console.WriteLine("I will love you either way");
